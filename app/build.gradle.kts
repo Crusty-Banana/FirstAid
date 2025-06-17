8c93c63f7a45f3c387da5f082688792a1b39930e
CrustyBanana <dodoankhac@gmail.com>
2025-06-17 08:31:03 +0700

    release modification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "io.livekit.android.example.voiceassistant"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
